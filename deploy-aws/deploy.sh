480073b0b717205274966965292323c82e9ae405
knawara <blueeyedhush@gmail.com>
2017-06-22 10:35:50 +0200

    enable human-readable output for ansible

--- deploy-aws/deploy.sh
+++ deploy-aws/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+export ANSIBLE_STDOUT_CALLBACK=debug
 ansible-playbook -i ec2-inventory.py -u ubuntu --tags deploy playbook.yml
--- deploy-aws/provision.sh
+++ deploy-aws/provision.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+export ANSIBLE_STDOUT_CALLBACK=debug
 ansible-playbook -i ec2-inventory.py --tags provision playbook.yml
